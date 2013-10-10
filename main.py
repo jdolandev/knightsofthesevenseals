646107da73fe7b4d30f887ba6ed3a03778520591
Joe Dolan <jdolandev@gmail.com>
2013-10-10 17:41:13 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,23 +2,34 @@ import random
 import os, sys
 import pygame
 
-inventory=[]
-dead= False
-location = "town"
+def main():
+	#initialize screen
+	pygame.init()
+	screen = pygame.display.set_mode((150,50))
+	pygame.display.set_caption("Knights of the Seven Seals")
 
-#Equipment
-iHead = None
-iArmR = None
-iArmL = None
-iChest = None
-iLegs = None
-iShoes = None
+	#Background
+	background = pygame.Surface(screen.get_size())
+	background = background.convert()
+	background.fill((0,0,0))
 
-screen = pygame.display.set_mode((640,480),32,0)
+	#Display Text
+	font = pygame.font.Font(None,36)
+	text = font.render("Hello World!", 1,(10,10,10))
+	textpos = text.get_rect()
+	textpos.centerx = background.get_rect().centerx
+	background.blit(text,textpos)
 
-pygame.init()
-while True:
-	for event in pygame.event.get():
-		if event.type == QUIT:
-			exit()
-	pygame.display.update()
+	#Blit everything to screen
+	screen.blit(background,(0,0))
+	pygame.disply.flip()
+
+	#Event loop
+	while 1:
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				exit()
+		screen.blit(background,(0,0))
+		pygame.display.flip
+
+if __name__=='__main__': main()
