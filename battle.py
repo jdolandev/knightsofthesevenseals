dd9a7563398b1ce5a6bb4927fd0affac1bbd23ed
Joe Dolan <jdolandev@gmail.com>
2013-10-20 16:44:48 -0400

    Update battle.py

--- battle.py
+++ battle.py
@@ -2,21 +2,16 @@ import random
 from enemy import *
 
 class Battle:
-	def __init__(self):
+	def __init__(self, location):
 		probability = random.randint(0,100)
-		if (location = "Field"):
+		self.enemy = None
+		if (location.lower() == "field"):
 			if (probability <= 40):
 				self.enemy = "Slime"
 				slime = Enemy("Slime","A slimy creature",20,10)
 			elif ((probability > 40) and (probability <= 50)):
-				self.enemy = "Theif"
+				self.enemy = "Thief"
 				thief = Enemy("Thief","A cunning trickster",30,15)
-			elif ((probability > 50) and (probability <= 55)):
+			else:
 				self.enemy = "Mimic"
 				mimic = Enemy("Mimic","Mistook for a treasure chest",15,40)
-		print(self.enemy)
-		yourTurn = True
-	def phases(self):
-		if not yourTurn:
-			self.enemy.attack()
-			yourTurn = True
